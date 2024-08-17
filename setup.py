4816b54fb101a377b915cafd48910707cc83b425
DevMehta-30 <dev.a.mehta30@gmail.com>
2024-08-17 16:20:42 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myws",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'myws=cli:main',
+        ],
+    },
+    author="Dev Mehta",
+    author_email="dev.a.mehta30@gmail.com",
+    description="Allowing users to create workspaces",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/diti-jain/gitsy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
